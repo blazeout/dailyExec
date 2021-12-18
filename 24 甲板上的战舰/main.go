9851bece35908112629894fc6fa762c7d26b6608
blazeout <1072830776@qq.com>
2021-12-18 23:31:14 +0800

    战舰, 搜索遍历

--- 24 甲板上的战舰/main.go
+++ 24 甲板上的战舰/main.go
@@ -1,6 +1,6 @@
 package main
 
-func countBattleships(board [][]byte) int {
+func countBattleships1(board [][]byte) int {
 	m, n := len(board), len(board[0])
 	ans := 0
 	for i, row := range board {
@@ -26,6 +26,21 @@ func countBattleships(board [][]byte) int {
 		}
 	}
 	return ans
+}
+
+func countBattleships(board [][]byte) (ans int) {
+	// 枚举战舰的左上顶点即可, 因为两艘战舰之间至少有一个水平或垂直的空位分隔
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			if board[i][j] == 'X' {
+				// 上面或者左边有板子就不要
+				if !(i > 0 && board[i-1][j] == 'X' || j > 0 && board[i][j-1] == 'X') {
+					ans++
+				}
+			}
+		}
+	}
+	return
 }
 
 func main() {
