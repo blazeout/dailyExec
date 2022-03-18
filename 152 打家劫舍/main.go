f3f824b89ce2129bf4629ab1d625f9c5d36dc11b
blazeout <1072830776@qq.com>
2022-03-18 10:18:36 +0800

    每日一题

---
+++ 152 打家劫舍/main.go
@@ -0,0 +1,30 @@
+package main
+
+func rob(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	} else if len(nums) == 2 {
+		return max(nums[0], nums[1])
+	}
+	// 偷今天和不偷今天的情况
+	// dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	// dp[1] 也许就是我可以选择拿第一天大的, 不拿今天的
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+	}
+	return dp[len(dp)-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+
+}
