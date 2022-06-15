c74372615bafad3a49bc6f78496f65d290db0440
blazeout <1072830776@qq.com>
2022-06-15 10:04:35 +0800

    practice

--- 198  删列造序/main.go
+++ 198  删列造序/main.go
@@ -1,4 +1,6 @@
-package _98__删列造序
+package main
+
+import "fmt"
 
 // 删列造序
 func minDeletionSize(strs []string) int {
@@ -16,4 +18,21 @@ next:
 		}
 	}
 	return count
+}
+
+type Student struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	arr := []int{1, 2, 3}
+	arr = append(arr, 4, 5, 6, 7)
+	fmt.Println(cap(arr), len(arr))
+	s1 := Student{"a", 1}
+	s2 := Student{"b", 2}
+	s3 := Student{"c", 3}
+	arr2 := []Student{s1}
+	arr2 = append(arr2, s2, s3)
+	fmt.Println(cap(arr2))
 }
